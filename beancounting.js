7e9606752b905a6774cf234e3912044e465339c0
Peter Jones <pjones15@gmu.edu>
2014-09-10 02:20:22 -0400

    almost

--- beancounting.js
+++ beancounting.js
@@ -1,10 +1,21 @@
 /*Bean Counting*/
 
 function countBs(string) {
-	var B = 0;
-  for(count=0; count = (string.length-1); count++);
+	var B = 0; 
+  for(var count=0; count < string.length; count++)
   if (string.charAt(count) == "B")
-  		return B + 1;
-};
+  		B = (B + 1);
 
+  	  	return B;
+};
 console.log(countBs("BBC"));
+
+function countChar(string, N) {
+	var B = 0; 
+  for(var count=0; count < string.length; count++)
+  if (string.charAt(count) == N)
+  		B = (B + 1);
+
+  	  	return B;
+};
+console.log(countChar("kakkerlak, "k"));
