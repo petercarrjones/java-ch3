dddc57ff230b3e494f159ac89b68a0a1081a4b6e
Peter Jones <pjones15@gmu.edu>
2014-09-10 00:58:11 -0400

    clean up recursion with left-hand fix and deleting unused var

--- recursion.js
+++ recursion.js
@@ -1,14 +1,12 @@
 /* Recursion */
 
 function isEven(number) {
-	var even = "Even"
-	var odd = "Odd"
     if (0 == number)
       return console.log("True");
     else if (1 == number)
       return console.log("False");
     else
-      return ((number-2)%==0)
+      return ((number-2)%2==0)
   }
 
-console.log(isEven(0));
+console.log(isEven(50));
