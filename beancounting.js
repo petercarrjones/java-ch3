2dfd21992dab22c798140cdc3ffc8cca2bad30f1
Peter Jones <pjones15@gmu.edu>
2014-09-10 01:39:05 -0400

    add function countBs

--- beancounting.js
+++ beancounting.js
@@ -1,5 +1,10 @@
 /*Bean Counting*/
 
-function countBs() {
-	
-}
+function countBs(string) {
+	var B = 0;
+  for(count=0; count = (string.length-1); count++);
+  if (string.charAt(count) == "B")
+  		return B + 1;
+};
+
+console.log(countBs("BBC"));
